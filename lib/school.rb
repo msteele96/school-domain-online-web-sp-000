f386a8b8799f9c0e4e094b0aa42641b9152d26e5
Marc Steele <marcsteele2014@gmail.com>
2020-10-20 16:22:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -25,7 +25,7 @@ end
 def sort
   sorted_roster = @roster
   @roster.map do |grade|
-    sorted_roster[grade] = @roster[grade].sort
+    sorted_roster[grade] = @roster[grade]
   end
   return sorted_roster
 
